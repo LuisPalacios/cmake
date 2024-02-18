ee32e9163487d4c0a84f8df44671db0be9401f46
Luis Palacios Derqui <luis.palacios.derqui@gmail.com>
2024-02-18 14:03:56 +0100

    Terminado 7_

--- 2_CMake/7_ExternalGit/app/main.cc
+++ 2_CMake/7_ExternalGit/app/main.cc
@@ -4,11 +4,18 @@
 #include "main.h"
 #include "my_lib.h"
 
+#include <nlohmann/json.hpp>
+
 int main() {
 
     print_hello_world();
     std::cout << "Hola mundo! yo soy main.cc" << std::endl;
     std::cout << "Proyecto: " << project_name << " v" << project_version << std::endl;
+
+    std::cout << "Nlohmann Json version "
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "." << std::endl;
 
     return 0;
 }
